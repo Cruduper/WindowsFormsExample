71fabe67506e35239f7199d9243ce02936d1d65f
Cruduper <briefcasemanx@gmail.com>
2024-11-25 21:18:41 -0800

    Adds functionality for storing a list of Employees objects created from user input, and displaying the list via popup dialog

--- EmployeeList/EmployeeList/Form1.Designer.cs
+++ EmployeeList/EmployeeList/Form1.Designer.cs
@@ -2,15 +2,14 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.Button buttonAddEmployee;
+        private System.Windows.Forms.Button buttonViewEmployees;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelID;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,21 +19,84 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.buttonAddEmployee = new System.Windows.Forms.Button();
+            this.buttonViewEmployees = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelID = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(100, 30);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(200, 20);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(100, 70);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(200, 20);
+            this.textBoxID.TabIndex = 1;
+            // 
+            // buttonAddEmployee
+            // 
+            this.buttonAddEmployee.Location = new System.Drawing.Point(100, 110);
+            this.buttonAddEmployee.Name = "buttonAddEmployee";
+            this.buttonAddEmployee.Size = new System.Drawing.Size(100, 30);
+            this.buttonAddEmployee.TabIndex = 2;
+            this.buttonAddEmployee.Text = "Add Employee";
+            this.buttonAddEmployee.UseVisualStyleBackColor = true;
+            this.buttonAddEmployee.Click += new System.EventHandler(this.buttonAddEmployee_Click);
+            // 
+            // buttonViewEmployees
+            // 
+            this.buttonViewEmployees.Location = new System.Drawing.Point(210, 110);
+            this.buttonViewEmployees.Name = "buttonViewEmployees";
+            this.buttonViewEmployees.Size = new System.Drawing.Size(100, 30);
+            this.buttonViewEmployees.TabIndex = 3;
+            this.buttonViewEmployees.Text = "View Employees";
+            this.buttonViewEmployees.UseVisualStyleBackColor = true;
+            this.buttonViewEmployees.Click += new System.EventHandler(this.buttonViewEmployees_Click);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(30, 33);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(38, 13);
+            this.labelName.TabIndex = 4;
+            this.labelName.Text = "Name:";
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(30, 73);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(21, 13);
+            this.labelID.TabIndex = 5;
+            this.labelID.Text = "ID:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.buttonViewEmployees);
+            this.Controls.Add(this.buttonAddEmployee);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.textBoxName);
+            this.Name = "Form1";
+            this.Text = "Employee List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
-
-        #endregion
     }
 }
 
--- EmployeeList/EmployeeList/Form1.cs
+++ EmployeeList/EmployeeList/Form1.cs
@@ -1,20 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace EmployeeList
 {
     public partial class Form1 : Form
     {
+        private List<Employee> employees;
+
         public Form1()
         {
             InitializeComponent();
+            employees = new List<Employee>();
         }
+
+        private void buttonAddEmployee_Click(object sender, EventArgs e)
+        {
+            string name = textBoxName.Text;
+            string id = textBoxID.Text;
+
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(id))
+            {
+                MessageBox.Show("Please enter both name and ID.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            employees.Add(new Employee { Name = name, ID = id });
+            textBoxName.Clear();
+            textBoxID.Clear();
+            MessageBox.Show("Employee added successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void buttonViewEmployees_Click(object sender, EventArgs e)
+        {
+            if (employees.Count == 0)
+            {
+                MessageBox.Show("No employees to display.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            string employeeList = "Current Employees:\n";
+            foreach (var employee in employees)
+            {
+                employeeList += $"Name: {employee.Name}, ID: {employee.ID}\n";
+            }
+
+            MessageBox.Show(employeeList, "Employee List", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
     }
 }
